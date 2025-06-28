b837f325242749db23adc6781c886e75981e3edf
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-06-28 15:31:29 +0300

    feat: add account schema definitions for account management

---
+++ internal/schemas/accounts.go
@@ -0,0 +1,18 @@
+package schemas
+
+type Account struct {
+	ID         string `json:"id"`
+	Platform   string `json:"platform"`
+	Identifier string `json:"identifier"`
+	URL        string `json:"url"`
+	Notes      string `json:"notes"`
+	Strength   int    `json:"strength"`
+}
+
+type AccountsListResponse []Account
+
+type IdentifiersResponse []string
+
+type AccountPassphraseResponse string
+
+type CreateAccountResponse Account
